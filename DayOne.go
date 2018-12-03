6c6a20ceb6b27d453de0374c97cf1e160788d370
David Hodgson <dahodgson@gmail.com>
2018-12-03 21:48:39 +0000

    Day One - Part Two Complete

--- DayOne.go
+++ DayOne.go
@@ -6,6 +6,30 @@ import (
 	"strings"
 )
 
+func calculateRepeatingFrequency(input string) int {
+	inputList := strings.Split(input, "\n")
+	frequency :=0
+	foundFrequency := make(map[int]int)
+	i := 0
+	for ;; {
+
+		change, _ := strconv.Atoi(inputList[i])
+		frequency = frequency + change
+
+		if  _,ok := foundFrequency[frequency]; ok {
+			break
+		} else {
+			foundFrequency[frequency] = 1
+		}
+		i++
+		if i>=len(inputList) {
+			i = 0
+		}
+	}
+
+	return frequency
+}
+
 func calculateFrequency(input string) int {
 
 	inputList := strings.Split(input, "\n")
@@ -35,6 +59,16 @@ func DayOnePartOne() {
 	input := ReadFile("day1-input.txt")
 
 	frequency := calculateFrequency(input)
+
+	fmt.Println("Frequency: ", frequency)
+}
+
+func DayOnePartTwo() {
+	fmt.Println("Day 1 - Part Two")
+
+	input := ReadFile("day1-input.txt")
+
+	frequency := calculateRepeatingFrequency(input)
 
 	fmt.Println("Frequency: ", frequency)
 }
