28ab56ecc2d9b93641a9e7609658a73f01406345
David Hodgson <dahodgson@gmail.com>
2018-12-05 11:44:29 +0000

    Day Three - Part One Complete

--- DayThree.go
+++ DayThree.go
@@ -25,7 +25,25 @@ func DayThreePartOne() {
 		boxList[i] = box
 	}
 
-	fmt.Println(boxList)
+	fabric := createFabric(1000)
+	fmt.Println(fabric)
+
+	for i := 0; i < len(boxList); i++ {
+		addBoxToFabric(fabric, boxList[i])
+	}
+
+	overlapCount := 0
+
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			if fabric[i][j] > 1 {
+				overlapCount++
+			}
+		}
+	}
+
+	fmt.Println("Overlap count: ", overlapCount)
+
 }
 
 func parseStringToBox(stringDescription string) box {
@@ -44,4 +62,26 @@ func parseStringToBox(stringDescription string) box {
 	box.width = width
 	box.height = height
 	return box
+}
+
+func createFabric(size int) [][]int {
+
+	fabric := make([][]int, size)
+
+	for i := 0; i < size; i++ {
+		fabric[i] = make([]int, size)
+	}
+
+	return fabric
+}
+
+func addBoxToFabric(fabric [][]int, box box) {
+
+	fmt.Println("Add box: ", box.id)
+
+	for i := box.left; i < box.left+box.width; i++ {
+		for j := box.top; j < box.top+box.height; j++ {
+			fabric[i][j] = fabric[i][j] + 1
+		}
+	}
 }
