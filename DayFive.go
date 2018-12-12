765aab091ae4473df4d7839044272bcecf42ad68
David Hodgson <dahodgson@gmail.com>
2018-12-12 18:11:46 +0000

    Day Five - Part Two Complete

--- DayFive.go
+++ DayFive.go
@@ -25,25 +25,74 @@ func DayFivePartOne() {
 	fmt.Println("Result length: ", len(output))
 }
 
+func DayFivePartTwo() {
+
+	input := ReadFile("day5-input.txt")
+
+	//input = "dabAcCaCBAcCcaDA"
+
+	minLength := len(input)
+	bestChar := ""
+
+	for i := 0; i < 26; i++ {
+
+		lowerChar := string('a' + byte(i))
+		upperChar := strings.ToUpper(lowerChar)
+
+		trimedString := strings.Replace(input, lowerChar, "", -1)
+		trimedString = strings.Replace(trimedString, upperChar, "", -1)
+		output := trimedString
+		for {
+			output = reduce(trimedString)
+			if len(output) == len(trimedString) {
+				break
+			}
+
+			trimedString = output
+		}
+
+		if len(output) < minLength {
+			minLength = len(output)
+			bestChar = lowerChar
+		}
+	}
+
+	fmt.Println("Result length: ", minLength)
+	fmt.Println("Best character: ", bestChar)
+}
+
 func reduce(input string) string {
 
-	output := ""
+	output := make([]byte, len(input))
+	newLength := 0
 	for i := 0; i < len(input)-1; i++ {
 
 		current := string(input[i])
 		next := string(input[i+1])
-		if current != next && (current == strings.ToUpper(next) ||
-			current == strings.ToLower(next)) {
-			output += input[0:i]
-			output += input[i+2:]
-			break
+		if current != next && (strings.ToUpper(current) == strings.ToUpper(next)) {
+			//We've found a set to skip this character
+			//and the next
+			i++
+			if i == len(input)-2 {
+				output[newLength] = input[i+1]
+				newLength++
+			}
+		} else {
+			//no match add this character
+			output[newLength] = input[i]
+			newLength++
 
+			if i == len(input)-2 {
+				output[newLength] = input[i+1]
+				newLength++
+			}
 		}
 
 	}
 
-	if output == "" {
-		output = input
+	outputString := string(output[0:newLength])
+	if outputString == "" {
+		outputString = input
 	}
-	return output
+	return outputString
 }
