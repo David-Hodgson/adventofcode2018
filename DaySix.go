c1081c7feabccbb9c433736d42ff3eb74eb7a8ac
David Hodgson <dahodgson@gmail.com>
2018-12-19 20:43:17 +0000

    Day Size - Part 2 Complete

--- DaySix.go
+++ DaySix.go
@@ -77,6 +77,77 @@ func DaySixPartOne() {
 	fmt.Println("Max blobsize: ", maxBlobSize)
 }
 
+func DaySixPartTwo() {
+	fmt.Println("Day 6 - Part Two")
+
+	input := strings.Split(ReadFile("day6-input.txt"), "\n")
+
+	pointList := make([]point, len(input))
+	maxX := 0
+	minX := 0
+	maxY := 0
+	minY := 0
+	for i := 0; i < len(input); i++ {
+		inputParts := strings.Split(input[i], ",")
+		x, _ := strconv.Atoi(inputParts[0])
+
+		if x > maxX {
+			maxX = x
+		}
+
+		if x < minX {
+			minX = x
+		}
+
+		y, _ := strconv.Atoi(strings.Trim(inputParts[1], " "))
+
+		if y > maxY {
+			maxY = y
+		}
+
+		if x < minY {
+			minY = y
+		}
+		newPoint := point{x, y}
+		pointList[i] = newPoint
+	}
+
+	fmt.Println(pointList)
+	fmt.Println("Max x: ", maxX)
+	fmt.Println("Min x: ", minX)
+	fmt.Println("Max y: ", maxY)
+	fmt.Println("Min y: ", minY)
+
+	regionSize := processPointsToFindTotalDistances(pointList, 10000, maxX, maxY)
+	fmt.Println("Region Size: ", regionSize)
+}
+
+func processPointsToFindTotalDistances(pointList []point, maxDistance int, maxX int, maxY int) int {
+	grid := make([][]int, maxX+(maxDistance*2))
+
+	regionSize := 0
+	for x := 0; x < maxX+(maxDistance*2); x++ {
+		grid[x] = make([]int, maxY+(maxDistance*2))
+	}
+
+	for x := 0; x < len(grid); x++ {
+		for y := 0; y < len(grid[x]); y++ {
+			for pointNumber := 0; pointNumber < len(pointList); pointNumber++ {
+				xDist := abs(pointList[pointNumber].x + maxDistance - x)
+				yDist := abs(pointList[pointNumber].y + maxDistance - y)
+				manDist := xDist + yDist
+				grid[x][y] = grid[x][y] + manDist
+			}
+
+			if grid[x][y] < maxDistance {
+				regionSize++
+			}
+		}
+	}
+
+	return regionSize
+}
+
 func processPoints(pointList []point, gridPadding int, maxX int, maxY int) []int {
 
 	blobSizes := make([]int, len(pointList))
