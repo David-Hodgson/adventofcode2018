8616c1b8a38d1b715b9cdff6bbcf959bfdf233bd
David Hodgson <dahodgson@gmail.com>
2018-12-20 15:25:37 +0000

    Day Seven - Part Two WIP

--- DaySeven.go
+++ DaySeven.go
@@ -50,3 +50,64 @@ func DaySevenPartOne() {
 
 	fmt.Println("Output: ", output)
 }
+
+
+func DaySevenPartTwo() {
+	fmt.Println("Day 7 - Part Two")
+
+	input := strings.Split(ReadFile("day7-input.txt"), "\n")
+
+	stateMap := make(map[string]map[string]bool)
+	for i := 0; i < len(input); i++ {
+
+		preStep := input[i][5:6]
+		step := input[i][36:37]
+		if _, exists := stateMap[step]; !exists {
+			stateMap[step] = make(map[string]bool)
+		}
+
+		if _, exists := stateMap[preStep]; !exists {
+			stateMap[preStep] = make(map[string]bool)
+		}
+
+		stateMap[step][preStep] = true
+	}
+
+	output := ""
+	currentTime := 0
+	maxWorkers := 5
+	workers := make(map[string]int)
+
+	for len(stateMap) > 0 {
+
+		for state,time := range workers {
+			workers[state] = time -1
+			//TODO check for done states
+			if workers[state] == 0 {
+				delete(workers, state)
+				delete(stateMap, state)
+				for _,preState := range stateMap {
+					delete(preState, state)
+				}
+				output += state
+				fmt.Println("output: ", output)
+			}
+		}
+		
+		if len(workers) < maxWorkers {
+			//Look for somethhing to do
+		}
+		currentTime++
+	}
+
+	for state, _ := range stateMap {
+
+		bob := int(state[0]) - 64
+		fmt.Println("State: ", state[0:1], ", Value: ", bob)	
+		
+		currentTime++
+	}	
+
+	fmt.Println("Elapsed Time: ", currentTime)
+	fmt.Println("Output: ", output)
+}
