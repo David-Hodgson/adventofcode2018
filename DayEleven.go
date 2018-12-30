fc0debbacda190d334deb06ff730fb95882a818d
David Hodgson <dahodgson@gmail.com>
2018-12-30 15:54:38 +0000

    Day 11 - Part 2 Complete

--- DayEleven.go
+++ DayEleven.go
@@ -49,3 +49,66 @@ func DayElevenPartOne() {
 	//fmt.Println("Grid: ", grid)
 	fmt.Println("Best power: ", largestTotalPower, " at x: ", bestX, ", y: ", bestY)
 }
+
+func DayElevenPartTwo() {
+	fmt.Println("Day 11 - Part 2")
+
+	serialNumber := 5468
+
+	grid := make([][]int, 300)
+	for i := 0; i < 300; i++ {
+		grid[i] = make([]int, 300)
+	}
+
+	for x := 1; x < 301; x++ {
+
+		for y := 1; y < 301; y++ {
+			rackId := x + 10
+			powerLevel := rackId * y
+			powerLevel += serialNumber
+			powerLevel = powerLevel * rackId
+			powerLevel = powerLevel % 1000
+			powerLevel = powerLevel / 100
+			powerLevel -= 5
+
+			grid[x-1][y-1] = powerLevel
+		}
+	}
+
+	calculateGridPower(grid)
+}
+
+func calculateGridPower(grid [][]int) {
+
+	largestTotalPower := 0
+	bestX := 0
+	bestY := 0
+	bestSize := 0
+
+	for squareSize := 1; squareSize <= 300; squareSize++ {
+		fmt.Println("Test squares of size: ", squareSize)
+		for x := 0; x <= len(grid)-squareSize; x++ {
+			for y := 0; y <= len(grid)-squareSize; y++ {
+
+				gridTotal := 0
+				for i := 0; i < squareSize; i++ {
+					for j := 0; j < squareSize; j++ {
+
+						gridTotal += grid[x+i][y+j]
+					}
+				}
+
+				if gridTotal > largestTotalPower {
+					largestTotalPower = gridTotal
+					bestX = x + 1
+					bestY = y + 1
+					bestSize = squareSize
+				}
+			}
+		}
+
+		fmt.Println("\tCurrent Best power: ", largestTotalPower, ", x: ", bestX, ", y: ", bestY, ", size: ", bestSize)
+	}
+
+	fmt.Println("Best power: ", largestTotalPower, ", x: ", bestX, ", y: ", bestY, ", size: ", bestSize)
+}
